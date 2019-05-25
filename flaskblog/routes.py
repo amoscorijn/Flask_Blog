b3ec01ef11d8c20ee8539020514b0e450e77610c
Junior <amoscorijn@gmail.com>
2019-05-25 16:48:21 -0700

    Last time

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect
-from flaskblog import app, db, bcrypt
+from flaskblog import app, db#, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
